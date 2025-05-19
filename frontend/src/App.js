1f8dec5cc9957a8d4326f2a97c29120dc3827ea7
SanjanaSoma11 <ssoma11@asu.edu>
2025-05-19 10:26:50 -0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QueryInterface from "./components/QueryInterface";
+import "./components/Query.css";
+
+function App() {
+  return (
+    <div className="App">
+      <QueryInterface />
+    </div>
+  );
+}
+
+export default App;
